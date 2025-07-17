58d7d7eb5ce0dbac0b85601db3e8d56b7b81456d
OriolEscSerr <oriolescserr@gmail.com>
2025-07-17 20:49:39 +0200

    Add implementation and documentation for the first assignment (done in another repository)

---
+++ introduction_to_embedded_systems_software_and_development_environments/module_1/application_assignment/README.md
@@ -0,0 +1,27 @@
+# Week 1 Application Assignment
+**Statistical Analysis in C**
+
+This project is the first assignment of Module 1 in the *Introduction to Embedded Systems Software and Development Environments* course (Coursera – University of Colorado Boulder, Prof. Alex Fosdick).
+
+The goal of the assignment is to get familiar with C programming, working with arrays and functions, compiling from the command line using GCC, and managing the project using Git.
+
+## What the Program Does
+
+The program works with a fixed array of 40 `unsigned char` values. It performs the following operations:
+
+- **Calculates and prints statistics**: minimum, maximum, mean, and median.
+- **Sorts the array** from largest to smallest.
+- **Prints the array** to the terminal before and after sorting.
+- Uses clean output formatting for all statistics and data.
+
+All statistical values are rounded down to the nearest integer.
+
+## Files Included
+
+- `stats.c` – Contains all function definitions.
+- `stats.h` – Contains function declarations and documentation.
+- `README.md` – Project description and metadata (this file).
+
+## Author
+
+Developed by **Oriol Escolà Serra** as part of the course requirements.
---
+++ introduction_to_embedded_systems_software_and_development_environments/module_1/application_assignment/stats.h
@@ -0,0 +1,113 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material.
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h
+ * @brief Function declarations for basic statistics operations.
+ *
+ * This header file contains the declarations of functions used to compute and
+ * display basic statistics (minimum, maximum, mean, median) on an array of
+ * data.
+ *
+ * @author Oriol Escolà Serra
+ * @date 16/07/2025
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/**
+ * @brief Prints the statistics of an array.
+ *
+ * This function prints the minimum, maximum, mean, and median values of an
+ * array.
+ *
+ * @param min Minimum value in the array.
+ * @param max Maximum value in the array.
+ * @param mean Average value of the array.
+ * @param median Median value of the array.
+ *
+ * @return void
+ */
+void print_statistics(int min, int max, double mean, double median);
+
+/**
+ * @brief Prints an array.
+ *
+ * Given an array of data and a length, prints the array to the screen.
+ *
+ * @param array Pointer to the array of data.
+ * @param length Number of elements in the array.
+ *
+ * @return void
+ */
+void print_array(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Finds the median of an array.
+ *
+ * This function calculates the median of an array of data.
+ *
+ * @param array Pointer to the array of data.
+ * @param length Number of elements in the array.
+ *
+ * @return The median value of the array.
+ */
+double find_median(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Finds the mean of an array.
+ *
+ * This function calculates the mean of an array of data.
+ *
+ * @param array Pointer to the array of data.
+ * @param length Number of elements in the array.
+ *
+ * @return The mean value of the array.
+ */
+double find_mean(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Finds the maximum of an array.
+ *
+ * This function calculates the maximum of an array of data.
+ *
+ * @param array Pointer to the array of data.
+ * @param length Number of elements in the array.
+ *
+ * @return The maximum value of the array.
+ */
+int find_maximum(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Finds the minimum of an array.
+ *
+ * This function calculates the minimum of an array of data.
+ *
+ * @param array Pointer to the array of data.
+ * @param length Number of elements in the array.
+ *
+ * @return The minimum value of the array.
+ */
+int find_minimum(unsigned char *array, unsigned int length);
+
+/**
+ * @brief Sorts an array.
+ *
+ * Sorts an array from largest to smallest using a simple sorting algorithm.
+ *
+ * @param array Pointer to the array of data.
+ * @param length Number of elements in the array.
+ *
+ * @return void
+ */
+void sort_array(unsigned char *array, unsigned int length);
+
+#endif /* __STATS_H__ */
