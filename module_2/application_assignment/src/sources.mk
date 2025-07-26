1963fd95e2de81908327066cc5608ff69fbc5f8f
OriolEscSerr <oriolescserr@gmail.com>
2025-07-26 12:26:36 +0200

    Define source files and include paths based on platform

--- module_2/application_assignment/src/sources.mk
+++ module_2/application_assignment/src/sources.mk
@@ -9,9 +9,24 @@
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES =
+ifeq ($(PLATFORM),MSP432)
 
-# Add your include paths to this variable
-INCLUDES = 
+SOURCES = 	./main.c \
+			./memory.c \
+		  	./interrupts_msp432p401r_gcc.c \
+		  	./startup_msp432p401r_gcc.c \
+		  	./system_msp432p401r.c
 
+INCLUDES = 	-I../include/common \
+			-I../include/msp432 \
+			-I../include/CMSIS
+
+else
+
+SOURCES =	./main.c \
+			./memory.c
+
+
+INCLUDES = 	-I../include/common
+
+endif
