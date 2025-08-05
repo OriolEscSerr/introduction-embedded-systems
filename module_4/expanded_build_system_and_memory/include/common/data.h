8cf9ebc943dac8161fbc8ac327d53dc6330b4d7e
Oriol Escolà Serra <oriolescserr@gmail.com>
2025-08-05 20:03:08 +0200

    Add file header comments and description to data.c/h

---
+++ module_4/expanded_build_system_and_memory/include/common/data.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * Copyright (C) 2025 by Oriol Escolà Serra
+ *
+ * This file was created as part of an educational project. You are free to 
+ * use, modify, and redistribute this file for personal or educational purposes.
+ *
+ * Do not submit this code as original work in the Coursera course 
+ * "Introduction to Embedded Systems Software and Development Environments" 
+ *
+ * The author is not liable for any misuse or academic dishonesty.
+ *****************************************************************************/
+/**
+ * @file data.h
+ * @brief Abstraction of data read and write operations
+ *
+ * This header file provides an abstraction of reading and
+ * writing to data via function calls. 
+ *
+ * @author Oriol Escolà Serra
+ * @date 05/08/2025
+ */
---
+++ module_4/expanded_build_system_and_memory/src/data.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Copyright (C) 2025 by Oriol Escolà Serra
+ *
+ * This file was created as part of an educational project. You are free to 
+ * use, modify, and redistribute this file for personal or educational purposes.
+ *
+ * Do not submit this code as original work in the Coursera course 
+ * "Introduction to Embedded Systems Software and Development Environments" 
+ *
+ * The author is not liable for any misuse or academic dishonesty.
+ *****************************************************************************/
+/**
+ * @file data.c
+ * @brief Abstraction of data read and write operations
+ *
+ * This implementation file provides an abstraction of reading and
+ * writing to data via function calls. There is also a globally
+ * allocated buffer array used for manipulation.
+
+ * @author Oriol Escolà Serra
+ * @date 05/08/2025
+ */
