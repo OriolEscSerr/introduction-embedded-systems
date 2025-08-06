98cdc03a98e3b16207063aa35371626102572b72
Oriol Escolà Serra <oriolescserr@gmail.com>
2025-08-06 16:30:51 +0200

    Add  -DCOURSE1 implementation

--- module_4/expanded_build_system_and_memory/src/main.c
+++ module_4/expanded_build_system_and_memory/src/main.c
@@ -22,12 +22,18 @@
  */
 #include "platform.h"
 #include "memory.h"
+#include "course1.h"
 
 #define MAX_LENGTH (10)
 char buffer[MAX_LENGTH];
 
 /* A pretty boring main file */
 int main(void) {
+  #ifdef COURSE1
+    course1();
+    return 0;
+  #endif
+
   unsigned int i;
   char value;
 
